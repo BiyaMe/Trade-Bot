f498b3de3d1d327b9084292ecd3ce2737fd17b37
BiyaMe <biyamesay19@gmail.com>
2026-01-20 23:02:28 +0300

    feat: Configuration and utilities

---
+++ config/settings.py
@@ -0,0 +1,19 @@
+
+# Strict competition constants
+ALLOWED_SYMBOLS = [
+    "cmt_btcusdt",
+    "cmt_ethusdt",
+    "cmt_solusdt",
+    "cmt_dogeusdt",
+    "cmt_xrpusdt",
+    "cmt_adausdt",
+    "cmt_bnbusdt",
+    "cmt_ltcusdt"
+]
+
+MAX_LEVERAGE = 20
+MIN_CONFIDENCE = 0.0
+MAX_CONFIDENCE = 1.0
+
+# System constraints
+TIMEFRAME = "5m"  # as per "3-10 minutes" horizon implies short term
